4465bd69b675e89a928584cdb0c2ee36afaa1a92
Renato Puškarić <ag.tropico@gmail.com>
2022-04-07 16:07:54 +0200

    Access Computed functions without parentheses in {{}}

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 new Vue({
     el: '#vue-app',
     data: {
-        name: '',
-        age: ''
+        fleet: 30,
+        s: 0,
+        m: 0
     },
     methods: {
+        /* addToFleetStarships: function () {
+            console.log('added a starship');
+            return this.s + this.fleet
+        },
+        addToFleetMotherships: function() {
+            console.log('added a mothership');
+            return this.m + this.fleet
+        } */
         
+    },
+    computed: {
+        addToFleetStarships: function () {
+            console.log('added a starship');
+            return this.s + this.fleet
+        },
+        addToFleetMotherships: function () {
+            console.log('added a mothership');
+            return this.m + this.fleet
+        }
     }
 });
