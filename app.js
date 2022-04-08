cdacafdfdaf2a2763f66d877ef68d049dd0d8897
renato.puskaric <renato.puskaric@fina.hr>
2022-04-08 14:05:48 +0200

    Changing CSS n dynamic data by classes

--- app.js
+++ app.js
@@ -1,34 +1,17 @@
-new Vue({
+git new Vue({
     el: '#vue-app',
     data: {
-        name: 'Njonjo',
-        job: 'Astronaut',
-        website: 'https://www.bing.com',
-        websiteTag: '<a href="https://www.google.com">Gooogle</a>',
-        age: 32,
-        x: 0,
-        y: 0
+        available: false,
+        nearby: false
+
     },
-    methods: {
-        greet: function (timeOfDay) {
-            return 'Good ' + timeOfDay + ' ' + this.name;
-        },
-        /* add: function () {
-        this.age++;
-    },
-        subtract: function () {
-            this.age--;
-        }, */
-        add: function (inc) {
-            this.age += inc;
-        },
-        subtract: function (dcr) {
-            this.age -= dcr;
-        },
-        updateXY: function (event) {
-            console.log(event);
-            this.x = event.offsetX;
-            this.y = event.offsetY;
+    methods: {},
+    computed: {
+        compClasses: function () {
+            return {
+                available: this.available,
+                nearby: this.nearby
+            }
         }
     }
 });
