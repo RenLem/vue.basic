8eb31d1e0056f47b7ac232c6fb127005313482cd
renato.puskaric <renato.puskaric@fina.hr>
2022-04-08 14:34:27 +0200

    If else in html by vue

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
-git new Vue({
+new Vue({
     el: '#vue-app',
     data: {
-        available: false,
-        nearby: false
+        error: false,
+        success: false
 
     },
     methods: {},
-    computed: {
-        compClasses: function () {
-            return {
-                available: this.available,
-                nearby: this.nearby
-            }
-        }
-    }
+    computed: {}
 });
