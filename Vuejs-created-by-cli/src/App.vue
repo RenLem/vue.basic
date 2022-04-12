6a2b736dcb3cd274b360c6363c85cba138eb655c
Renato Puškarić <ag.tropico@gmail.com>
2022-04-12 15:19:21 +0200

    How do vue app functiones

---
+++ Vuejs-created-by-cli/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3>{{ title }}</h3>
+    <p>{{ greeting() }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data () {
+    return {
+      title: 'First Vue - woooo!'
+    }
+  },
+  methods: {
+    greeting: function () {
+      return 'Yipy kay yey! &;0)'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
